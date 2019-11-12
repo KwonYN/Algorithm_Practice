bf0e8df144d1503c57437f94e9ff8fb2ef5fe070
KwonYN <54017486+KwonYN@users.noreply.github.com>
2019-11-12 21:41:49 +0900

    Update (다시 해야 하뮤)새로운게임.c

--- RE/(다시 해야 하뮤)새로운게임.c
+++ RE/(다시 해야 하뮤)새로운게임.c
@@ -6,11 +6,12 @@
 
 enum COLOR{ W = 0, R = 1, B = 2 };
 
-typedef struct { int num, y, x, dir, prev, next; }HORSE;
-HORSE horse[12];
+struct _st { int num, y, x, dir;  struct _st * prev, * next; }HORSE;
+struct _st horse[12];
 
-//typedef struct { int top, idx[10], sp; }CHK;
-//CHK chk[MAX][MAX];
+typedef struct { int idx[10], sp; }CHK;
+CHK chk[MAX][MAX];
+
 
 void Input(void);
 int Operation(void);
@@ -18,10 +19,11 @@ void Init_Chk(void);
 
 void Print_HORSE(void);
 void Print_Chk(void);
+void Print_Structure(void);
 
 int N, K;
 int map[MAX][MAX];
-int chk[MAX][MAX];
+//int chk[MAX][MAX];
 int dy[5] = { 0, 0, 0, -1, 1 };
 int dx[5] = { 0, 1, -1, 0, 0 };
 int dd[5] = { 0, 2, 1, 4, 3 };
@@ -41,12 +43,13 @@ void Input(void)
 	int i, j;
 	int yy, xx, dd;
 
+	horse[0].num = 0;
 	scanf("%d %d", &N, &K);
 	for (i = 1; i <= N; i++) for (j = 1; j <= N; j++) scanf("%d", &map[i][j]);
 	for (i = 1; i <= K; i++)
 	{
 		scanf("%d %d %d", &yy, &xx, &dd);
-		horse[i] = (HORSE){i, yy, xx, dd, 0, 0};
+		horse[i] = (struct _st){ i, yy, xx, dd, (struct _st *)0, (struct _st * )0 };
 	}
 	for (i = 1; i <= N; i++) map[i][0] = map[N+1][0] = map[0][i] = map[0][N+1] = B;
 }
@@ -55,16 +58,16 @@ int Operation(void)
 {
 	int i, j, k, t = 0;
 	int d, sy, sx, ny, nx;
-	HORSE * head;
+	struct _st * head, *down, *up;
 	int tmp_idx[MAX];
-	int down, up, end_flag = 0;
+	int end_flag = 0;
 	
 	while (++t <= 1000)
 	{
 		for (i = 1; i <= K; i++)
 		{
-			head = (HORSE *)0x0;
-			if (horse[i].prev) continue;	// 다른 말 위에 있는 말은 움직이지 않는다. 밑에 있는 말 따라서 움직여짐.
+			head = (struct _st *)0x0;
+			if (horse[i].prev != (struct _st *)0x0) continue;	// 다른 말 위에 있는 말은 움직이지 않는다. 밑에 있는 말 따라서 움직여짐.
 			d = horse[i].dir;							// 가장 밑에 있는 말 i의 방향
 			sy = horse[i].y;		sx = horse[i].x;	// 가장 밑에 있는 말 i의 좌표
 			ny = sy + dy[d];		nx = sx + dx[d];	// 가장 밑에 있는 말 i의 이동
@@ -79,56 +82,40 @@ int Operation(void)
 			}// 위까지의 경우에 해당되지 않는다면 가장 밑에 있는 말 i가 움직였는데 그 곳이 하얀색이거나 빨간색인 경우!!
 			
 			horse[i].dir = d;	// 일단 말 i가 이동한 방향을 다시 말 i에 넣어줌
-			for (head = &horse[i], j = 1; head->next != 0; head = &horse[head->next], j++)// 말 i의 위에 있는 말들 모두 이동시켜줌!!
+
+			// 말 i의 위에 있는 말들 모두 이동시켜줌!!
+			for (head = &horse[i], j = 1; head; head = head->next, j++)
 			{
-				printf("head.num = %d\n", head->num);
-				getch();
-				horse[head->num].y = ny;		horse[head->num].x = nx;
-				tmp_idx[j] = head->num;	// 그와 동시에 말 i를 포함하여 그 위에 있는 말들의 idx를 배열에 넣어줌
+				tmp_idx[j] = head->num;// 그와 동시에 말 i를 포함하여 그 위에 있는 말들의 idx를 배열에 넣어줌
+				horse[head->num].y = ny;
+				horse[head->num].x = nx;
 			}
-
-			for (int z = 1; z < j; z++)
-			{
-				printf("%d ", tmp_idx[z]);
-			}
-			printf("\n");
-
-
-			/*
 			if (j >= 5)
 			{
 				end_flag = 1;
 				break;
 			}
-			if (map[ny][nx] == R)	// 빨간 색이라면, 거꾸로 넣어주어야 함!
+			if (map[ny][nx] == R)
 			{
-				down = chk[ny][nx];
 				for (k = j - 1; k >= 1; k--)
 				{
-					horse[tmp_idx[k]].prev = down;
-					down = horse[tmp_idx[k]].prev;
+					chk[ny][nx].idx[++chk[ny][nx].sp] = tmp_idx[k];
 				}
-				down = 0;
-				for (k = j - 1; k >= 1; k--)
-				{
-					horse[tmp_idx[k]].next = down;
-					down = horse[tmp_idx[k]].next;
-				}
-				chk[ny][nx] = horse[tmp_idx[j - 1]].num;
 			}
 			else
 			{
-				horse[i].prev = chk[ny][nx];
-				chk[ny][nx] = horse[tmp_idx[j - 1]].num;
+				for (k = 1; k < j; k++)
+				{
+					chk[ny][nx].idx[++chk[ny][nx].sp] = tmp_idx[k];
+				}
 			}
-			*/
-
-			/*
+			
 			Print_Chk();
 			getch();
-			*/
 
 		}
+		Print_Structure();
+		getch();
 		if (end_flag) break;
 		//초기화 및 처리 코드 함수!!!
 		Init_Chk();
@@ -140,9 +127,28 @@ int Operation(void)
 
 void Init_Chk(void)
 {
-	int i;
+	int i, k;
+	int yy, xx, sp_tmp;
+	struct _st * tmp = (struct _st *)0x0;
 
-	for (i = 1; i <= K; i++) chk[horse[i].y][horse[i].x] = 0;
+	for (i = 1; i <= K; i++)
+	{
+		yy = horse[i].y;		xx = horse[i].x;
+		if (chk[yy][xx].sp == 0) continue;
+		if (chk[yy][xx].sp <= 1) chk[yy][xx].sp = 0;
+		else
+		{
+			sp_tmp = chk[yy][xx].sp;
+			horse[chk[yy][xx].idx[1]].prev = (struct _st *)0x0;
+			for (k = 1; k <= sp_tmp; k++)
+			{
+				//horse[chk[yy][xx].idx[k]].prev;
+				horse[chk[yy][xx].idx[k]].next = &horse[chk[yy][xx].idx[k+1]];
+			}
+			horse[chk[yy][xx].idx[sp_tmp]].next = (struct _st *)0x0;
+			chk[yy][xx].sp = 0;
+		}
+	}
 }
 
 void Print_HORSE(void)
@@ -165,9 +171,20 @@ void Print_Chk(void)
 	{
 		for (j = 1; j <= N; j++)
 		{
-			printf("%d ", chk[i][j]);
+			printf("%d ", chk[i][j].sp);
 		}
 		printf("\n");
+	}
+	printf("\n");
+}
+
+void Print_Structure(void)
+{
+	int i;
+	printf("Print_Structure\n");
+	for (i = 1; i <= K; i++)
+	{
+		printf("[%d] y = %d, x = %d, dir = %d\n", horse[i].num, horse[i].y, horse[i].x, horse[i].dir);
 	}
 	printf("\n");
 }
